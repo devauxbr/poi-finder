2796f1d9cd079a88f062dc5c3d93c5e0a623e21e
Bruno Devaux <devaux.br@gmail.com>
2020-06-20 14:46:49 +0200

    create failing cli tests

--- cli/src/test/java/io/devaux/poifinder/PoiFinderCliApplicationTest.java
+++ cli/src/test/java/io/devaux/poifinder/PoiFinderCliApplicationTest.java
@@ -1,12 +1,51 @@
 package io.devaux.poifinder;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PoiFinderCliApplicationTest {
-    @Test
-    void should() {
+    // Setup inspired from https://stackoverflow.com/questions/1119385
+    private ByteArrayOutputStream outContent;
+    private ByteArrayOutputStream errContent;
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
 
+    @BeforeEach
+    public void setUpStreams() {
+        outContent = new ByteArrayOutputStream();
+        errContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    /*
+    These tests validate the answers to both questions from the exercise
+    They are HIGHLY dependent on JSON serialization configuration (line break, tabs, etc.) and will easily break !
+     */
+
+    @Test
+    void shouldGetPoiNumber() {
+        PoiFinderCliApplication.main(new String[]{"--nbpoi", "{\"min_lat\": 6.5, \"min_lon\": -7}"});
+        assertThat(outContent.toString()).isEqualTo("{\n\"value\": 2 }\n");
+        assertThat(errContent.toString()).isEmpty();
+    }
+
+    @Test
+    void shouldGetDensestZones() {
+        PoiFinderCliApplication.main(new String[]{"--densest", "{\"n\": 2}"});
+        assertThat(outContent.toString()).isEqualTo("[{\"min_lat\": -2.5, \"max_lat\": -2, \"min_lon\": 38, \"max_lon\": 38.5}, {\"min_lat\": 6.5, \"max_lat\": 7, \"min_lon\": -7, \"max_lon\": -6.5}]");
+        assertThat(errContent.toString()).isEmpty();
     }
 }
